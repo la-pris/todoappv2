61b173e7e6fa35123735bab02e3065a4b512f6aa
la-pris <priscillo.manuel@lawadvisor.com>
2022-01-23 23:58:56 +0800

    using tailwind with @apply still not working

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :todoapp, TodoappWeb.Endpoint,
   secret_key_base: "+8VSlImBFIHtDaNmoHuE5RizXZrzZSVJD21aYUKhwpj8UIfk4oKizoTmvodmiBQF",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
