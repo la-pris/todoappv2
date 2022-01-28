3071d513a36e2f1e3153bc718d63fdad3ffc8733
la-pris <priscillo.manuel@lawadvisor.com>
2022-01-28 10:56:47 +0800

    mvp of liveview app without css

--- lib/todoapp/todo/item.ex
+++ lib/todoapp/todo/item.ex
@@ -1,6 +1,9 @@
 defmodule Todoapp.Todo.Item do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+  alias Todoapp.Repo
+  alias __MODULE__
 
   schema "items" do
     field :person_id, :integer
@@ -14,6 +17,28 @@ defmodule Todoapp.Todo.Item do
   def changeset(item, attrs) do
     item
     |> cast(attrs, [:text, :person_id, :status])
-    |> validate_required([:text, :person_id, :status])
+    |> validate_required([:text])
+  end
+
+  def create_item(attrs \\ %{}) do
+    %Item{}
+    |> changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_item!(id), do: Repo.get!(Item, id)
+
+  def list_items do
+    Repo.all(Item)
+  end
+
+  def update_item(%Item{} = item, attrs) do
+    item
+    |> Item.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_item(%Item{} = item) do
+    Repo.delete(item)
   end
 end
--- lib/todoapp_web/live/todo_live.ex
+++ lib/todoapp_web/live/todo_live.ex
@@ -1,7 +1,8 @@
 defmodule TodoappWeb.TodoLive do
   use TodoappWeb, :live_view
 
-  alias Todoapp.Todo
+  alias Todoapp.Todo.Item
+
   @topic "live"
 
   # Things I need to do
@@ -10,17 +11,17 @@ defmodule TodoappWeb.TodoLive do
   # handle_event
   # draggable class
 
-
+  @impl true
   def mount(_params, _session, socket) do
     #this is a map with a key of todo
-    if connected?(socket), do: LiveViewTodoWeb.Endpoint.subscribe(@topic)
-    {:ok, assign(socket, items: Todo.list_items())}
+    if connected?(socket), do: TodoappWeb.Endpoint.subscribe(@topic)
+    {:ok, assign(socket, items: Item.list_items())}
   end
 
-
+  @impl true
   def handle_event("create", %{"text" => text}, socket) do
-    Todo.create_item(%{text: text})
-    socket = assign(socket, items: Todo.list_items(), active: %Todo{})
+    Item.create_item(%{text: text})
+    socket = assign(socket, items: Item.list_items(), active: %Item{})
     TodoappWeb.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
     {:noreply, socket}
   end
@@ -33,5 +34,13 @@ defmodule TodoappWeb.TodoLive do
   @impl true
   def handle_info(%{event: "update", payload: %{items: items}}, socket) do
     {:noreply, assign(socket, items: items)}
+  end
+
+  @impl true
+  def handle_event("delete", data, socket) do
+    Item.delete_item(Map.get(data, "id"))
+    socket = assign(socket, items: Item.list_items(), active: %Item{})
+    TodoappWeb.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
+    {:noreply, socket}
   end
 end
